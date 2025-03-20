62094117983a0aa3b14142ac50a3a08f1ee8a68b
isburnett <ian.sol.burnett@gmail.com>
2025-03-20 17:10:04 -0400

    Delete proxy-api.js

--- proxy-api.js
+++
@@ -1,5 +0,0 @@
-// Fetching data through the proxy server
-fetch("https://localhost:8443/https://api.example.com/data")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error("Error:", error));
